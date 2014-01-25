c61cf5d25aef0b95bc38d19cd8625c1c7d383533
Chip Castle <chip@chipcastle.com>
2014-01-25 16:19:46 -0600

    Add first web app using Morsel

---
+++ app.jl
@@ -0,0 +1,13 @@
+using Morsel
+
+app = Morsel.app()
+
+route(app, GET | POST | PUT, "/") do req, res
+    "This is the root"
+end
+
+get(app, "/about") do req, res
+    "This app is running on Morsel"
+end
+
+start(app, 8000)
